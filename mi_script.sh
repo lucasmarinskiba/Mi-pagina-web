f17436e7d04fc059329f35e70d19317e835eccd6
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-03-17 21:02:23 -0300

    Create mi_script.sh

---
+++ mi_script.sh
@@ -0,0 +1,7 @@
+cd lucas-marin-website
+git add .
+git commit -m "Descripción de los cambios"
+git push origin main
+git remote add origin https://github.com/tuusuario/lucas-marin-website.git
+git branch -M main
+git push -u origin main
